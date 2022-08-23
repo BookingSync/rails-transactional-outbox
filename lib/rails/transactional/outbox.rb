bffb021a2a00bcd65d75b5526a37da4a05cedd44
Karol Galanciak <karol.galanciak@gmail.com>
2022-08-23 16:53:24 +0200

    initial commit

---
+++ lib/rails/transactional/outbox.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "outbox/version"
+
+module Rails
+  module Transactional
+    module Outbox
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/transactional/outbox/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Rails
+  module Transactional
+    module Outbox
+      VERSION = "0.1.0"
+    end
+  end
+end
