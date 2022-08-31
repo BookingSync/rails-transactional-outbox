c2b9ca4962ac76364ff416847cd591ea2659a5ef
Karol Galanciak <karol.galanciak@gmail.com>
2022-08-31 08:39:14 +0200

    attempt to fix railtie issue

--- lib/rails_transactional_outbox/railtie.rb
+++ lib/rails_transactional_outbox/railtie.rb
@@ -2,6 +2,8 @@
 
 class RailsTransactionalOutbox
   class Railtie < Rails::Railtie
+    railtie_name :rails_transactional_outbox
+
     rake_tasks do
       load "tasks/rails_transactional_outbox.rake"
     end
