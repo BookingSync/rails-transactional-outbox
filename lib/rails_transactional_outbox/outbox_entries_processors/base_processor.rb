b53b16aaf3584c35365581bf89ef40f2084ac4f0
Karol Galanciak <karol.galanciak@gmail.com>
2022-09-08 13:54:20 +0200

    refactoring

---
+++ lib/rails_transactional_outbox/outbox_entries_processors/base_processor.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class RailsTransactionalOutbox
+  class OutboxEntriesProcessors
+    class BaseProcessor
+      attr_reader :config
+      private     :config
+
+      def initialize(config: RailsTransactionalOutbox.configuration)
+        @config = config
+      end
+
+      def call(&block)
+        return [] unless outbox_model.any_records_to_process?
+
+        execute(&block)
+      end
+
+      private
+
+      delegate :outbox_model, :outbox_batch_size, to: :config
+
+      def execute(&_block)
+        raise "implement me"
+      end
+
+      def process_records(records_to_process, &block)
+        failed_records = []
+        records_to_process.each do |record|
+          begin
+            process(record)
+          rescue => e
+            record.handle_error(e)
+            record.save!
+            failed_records << record
+          end
+          yield record if block
+        end
+        processed_records = records_to_process - failed_records
+        mark_as_processed(processed_records)
+      end
+
+      def process(record)
+        record_processor.call(record)
+      end
+
+      def record_processor
+        @record_processor ||= RailsTransactionalOutbox::RecordProcessor.new
+      end
+
+      def mark_as_processed(processed_records)
+        outbox_model.mark_as_processed(processed_records)
+      end
+    end
+  end
+end
--- lib/rails_transactional_outbox/outbox_entries_processors/ordered_by_causality_key_processor.rb
+++ lib/rails_transactional_outbox/outbox_entries_processors/ordered_by_causality_key_processor.rb
@@ -2,17 +2,12 @@
 
 class RailsTransactionalOutbox
   class OutboxEntriesProcessors
-    class OrderedByCausalityKeyProcessor
-      attr_reader :config
-      private     :config
+    class OrderedByCausalityKeyProcessor < RailsTransactionalOutbox::OutboxEntriesProcessors::BaseProcessor
+      private
 
-      def initialize(config: RailsTransactionalOutbox.configuration)
-        @config = config
-      end
+      delegate :lock_client, :lock_expiry_time, to: :config
 
-      def call(&block)
-        return [] unless outbox_model.any_records_to_process?
-
+      def execute(&block)
         unprocessed_causality_keys.each_with_object([]) do |causality_key, processed_records|
           lock_client.lock(lock_name(causality_key), lock_expiry_time) do |locked|
             next unless locked
@@ -21,10 +16,6 @@ class RailsTransactionalOutbox
           end
         end
       end
-
-      private
-
-      delegate :outbox_model, :outbox_batch_size, :lock_client, :lock_expiry_time, to: :config
 
       def unprocessed_causality_keys
         outbox_model.unprocessed_causality_keys
@@ -36,34 +27,6 @@ class RailsTransactionalOutbox
 
       def fetch_records(causality_key)
         outbox_model.fetch_processable_for_causality_key(outbox_batch_size, causality_key).to_a
-      end
-
-      def process_records(records_to_process, &block)
-        failed_records = Concurrent::Array.new
-        records_to_process.each do |record|
-          begin
-            process(record)
-          rescue => e
-            record.handle_error(e)
-            record.save!
-            failed_records << record
-          end
-          yield record if block
-        end
-        processed_records = records_to_process - failed_records
-        mark_as_processed(processed_records)
-      end
-
-      def process(record)
-        record_processor.call(record)
-      end
-
-      def record_processor
-        @record_processor ||= RailsTransactionalOutbox::RecordProcessor.new
-      end
-
-      def mark_as_processed(processed_records)
-        outbox_model.mark_as_processed(processed_records)
       end
     end
   end
